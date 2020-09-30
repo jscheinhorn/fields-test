53172cdd26b77ee2b8bf1596a85b9058af3d5a89
Jennifer Scheinhorn <jscheinhorn@paypal.com>
2020-09-30 16:24:53 -0400

    fix: update server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const { join } = require('path')
-const openBrowser = require('react-dev-utils/openBrowser')
+// const openBrowser = require('react-dev-utils/openBrowser')
 
 const app = express()
 
 app.use(express.static('public'))
 
-app.listen(8080)
+app.listen(process.env.PORT || 8080, function() {
+  console.log(`App listening on port!`)
+})
 
-if (openBrowser('http://localhost:8080')) {
-  console.log('The browser tab has been opened!')
-}
+// if (openBrowser('http://localhost:8080')) {
+//   console.log('The browser tab has been opened!')
+// }
