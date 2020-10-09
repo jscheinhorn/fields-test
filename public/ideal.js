499a3a0860a9d0ee37029472b7595b7211448cf7
Jennifer Scheinhorn <jscheinhorn@paypal.com>
2020-10-09 14:00:05 -0400

    chore: separate idealSdk.js code, clean up

--- public/ideal.js
+++ public/ideal.js
@@ -2,30 +2,6 @@
 const preFill = sessionStorage.preFill
 console.log({ preFill })
 console.log('sessionStorage.environment: ', sessionStorage.environment)
-// payPalSdk = {
-//   sandbox:
-//     'paypal.com/sdk/js?client-id=AWpNai5MkBQDlnUmeYU02YHGOEkUs-ib8ufPtamRXZ_Uc8BuRJ6gCcaBZ-IVKBCBDtuw_7JRmbmdbERa&components=buttons,fields,marks&buyer-country=NL&currency=EUR',
-//   msmaster:
-//     'msmaster.qa.paypal.com/sdk/js?client-id=AVVSS5kWC3KEdory_C7uev8yYIZyemM4BQC9tt-koQDL5iRgjTAkpypPaE29mEGy1eRFCAEOjGYWN1TC&components=buttons,fields,marks&buyer-country=NL&currency=EUR',
-//   testEnv:
-//     '.qa.paypal.com/sdk/js?client-id=AVVSS5kWC3KEdory_C7uev8yYIZyemM4BQC9tt-koQDL5iRgjTAkpypPaE29mEGy1eRFCAEOjGYWN1TC&components=buttons,fields,marks&buyer-country=NL&currency=EUR',
-// }
-// let src = 'https://www.'
-// switch (sessionStorage.environment) {
-//   case 'sandbox':
-//     src += payPalSdk.sandbox
-//     break
-//   case 'msmaster':
-//     src += payPalSdk.msmaster
-//     break
-//   default:
-//     src += sessionStorage.environment + payPalSdk.testEnv
-// }
-// console.log({ src })
-
-// let script = document.createElement('SCRIPT')
-// script.src = src
-// const head = document.getElementsByTagName('HEAD')[0].after(script)
 
 let name = ''
 if (preFill === 'true') {
