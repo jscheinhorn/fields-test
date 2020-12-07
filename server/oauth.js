a509d196273a1b254c709cb7f3a78215ff884455
Jennifer Scheinhorn <jscheinhorn@paypal.com>
2020-12-07 12:27:55 -0500

    chore: refactor oauth for sandbox and live (for heroku)

--- server/oauth.js
+++ server/oauth.js
@@ -5,26 +5,38 @@ const fetch = require('node-fetch')
 
 dotenv.config()
 
-// const PAYPAL_CLIENT_ID = process.env.PAYPAL_CLIENT_ID
-// const PAYPAL_CLIENT_SECRET = process.env.PAYPAL_CLIENT_SECRET
+let bearer
+let PAYPAL_CLIENT_ID
+let PAYPAL_CLIENT_SECRET
 const MERCHANT_AUTH_CODE = process.env.PP_STAGE_MERCH_AUTH_CODE
 
-// const PROD = "https://api.paypal.com";
-// const SANDBOX = "https://api.sandbox.paypal.com";
+let BASE_URL
+const PROD = 'https://api.paypal.com'
+const SANDBOX = 'https://api.sandbox.paypal.com'
 const STAGE = `https://api.msmaster.qa.paypal.com`
 
-const BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? PROD
-    : process.env.NODE_ENV === 'sandbox'
-    ? SANDBOX
-    : STAGE
+switch (process.env.NODE_ENV) {
+  case 'production':
+    BASE_URL = PROD
+    PAYPAL_CLIENT_ID = process.env.PP_LIVE_CLIENT_ID_WEBHOOK
+    PAYPAL_CLIENT_SECRET = process.env.PP_LIVE_CLIENT_SECRET_WEBHOOK
+    bearer = `${btoa(`${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`)}`
+    break
+  case 'sandbox':
+    BASE_URL = SANDBOX
+    PAYPAL_CLIENT_ID = process.env.PP_SANDBOX_CLIENT_ID_WEBHOOK
+    PAYPAL_CLIENT_SECRET = process.env.PP_SANDBOX_CLIENT_SECRET_WEBHOOK
+    bearer = `${btoa(`${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`)}`
+    break
+  default:
+    BASE_URL = STAGE
+    bearer = MERCHANT_AUTH_CODE
+    break
+}
 
 // Get an OAuth token for the PayPal Orders API using the credentials included in the .env file.
 // https://developer.paypal.com/docs/api/get-an-access-token-curl/
 async function getAuthToken() {
-  // const bearer = `${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`
-
   const response = await fetch(`${BASE_URL}/v1/oauth2/token`, {
     body: 'grant_type=client_credentials',
     headers: {
